cb27d218f7322701448656e5b5ea3f50b73e65a7
ShaliniEOS <shalinipanguluri2122@gmail.com>
2021-12-14 12:45:29 +0530

    question 4

--- p2.c
+++ p2.c
@@ -8,19 +8,27 @@
 int main(int argc,char const *argv[])
 {
     unsigned char buff[466472];
+    int fd1[2],fd2[2];
+    pid_t id;
+    id=fork();
     int fdr;
     int i;
     fdr=open("dictionary.txt",O_RDONLY);
     read(fdr,buff,466472);
     for(i=0;i<3000;i++)
+    close(fd1[1])
     {
+        read(fd1[0],str,10);
         if(buff[i]=="\n")
         {
             printf("%c",buff[i]);
             printf("-%ld\n",strlen(buff[i]));
-
+            
         }
     }
+    close(fd1[0]);
+    close(fd2[0]);
+   write(fd2[0],sr,strlen(str)+1);
     close(fdr);
     return 0;
 }
